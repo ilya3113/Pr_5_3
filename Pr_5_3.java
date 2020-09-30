f1807fa45ce84106ad1173d09bf5f3f8563565bb
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2020-09-30 20:04:08 +0300

    Add files via upload

---
+++ Pr_5_3.java
@@ -0,0 +1,21 @@
+package ru.mirea.Lab5.Pr_5_3;
+
+public class Pr_5_3 {
+    public static String recursion(int a, int b) {
+        if (a > b) {
+            if (a == b) {
+                return Integer.toString(a);
+            }
+            return a + " " + recursion(a - 1, b);
+        } else {
+            if (a == b) {
+                return Integer.toString(a);
+            }
+            return a + " " + recursion(a + 1, b);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(5, 0));
+        System.out.println(recursion(0, 5));
+    }
+}
